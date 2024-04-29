e9637f94535f0a0b130b5e26f98b7c4c5a778843
arnav <arnabdas2753@gmail.com>
2024-04-29 12:06:39 +0530

    signup works

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("database is working");
@@ -10,3 +12,7 @@ const app= express();
 app.listen(3000, ()=>{
   console.log("server is running")
 })
+app.use(express.json());
+
+app.use('/api/user', userRouter);
+app.use('/api/auth',authRouter);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
+import { Features } from './Components/Features'; // Import Features component instead of FeatureCard
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-   <Home />
+    <Home />
     <App />
+    <Features /> {/* Render the Features component instead of FeatureCard */}
     <Footer />
   </BrowserRouter>
 );
