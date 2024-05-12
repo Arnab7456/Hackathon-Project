1a0538792d66a038b1eec4cb41568567067c9af1
arnav <arnabdas2753@gmail.com>
2024-05-13 01:25:26 +0530

    basic chatbot add

--- frontend/src/Pages/Body.jsx
+++ frontend/src/Pages/Body.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import VideoPlayer from "../Pages/VideoPlayer";
 import { Features } from '../Components/Features';
+import Chatbot from '../Components/Chatbot';
 const Body = () => {
   return (
     <div>
       <VideoPlayer></VideoPlayer>
       <Features />
+      <Chatbot />
     </div>
   )
 }
